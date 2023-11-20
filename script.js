c16f3e8d427266e03236ada914c3e8d7bab750c3
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-11-20 13:57:59 +0100

    added home button to navbar

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // nav bar animation
 window.addEventListener("scroll", () => {
    const navBar = document.querySelector("nav");
-   navBar.classList.toggle("sticky", window.scrollY > 0);   
+   navBar.classList.toggle("sticky", window.scrollY > 0); 
+   // show home element only when scrolled 
+   const home = document.querySelector("#home");
+   home.classList.toggle("show", window.scrollY > 0);
 })
